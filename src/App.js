80db07dc5730cfc224a55c778b1047fe2924fb08
Sikal Sikal <sikal.sikal.ss@gmail.com>
2021-01-26 21:16:59 +0100

    Finished building the sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import './App.css';
 import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Videos from "./components/Videos/Videos";
 
 function App() {
   return (
     <div className="app">
         <Header />
         <div className="app__page">
-
+            <Sidebar />
+            <Videos />
         </div>
     </div>
   );
